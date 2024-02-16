de12c0443a49bde7b04d8eca1a12a474c59e7ee4
Joobee <joobee97@gmail.com>
2024-02-15 16:48:19 -0800

    added test python file

--- pages/0_Test.py
+++ pages/0_Test.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.title("TL;DW")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.caption("🚀 Get a TED Talk Recommendation based on your interest!")
+
+title = st.text_input('Youtube URL', 'Input your interested YT video!')
