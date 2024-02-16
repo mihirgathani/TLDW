ff05839911baa6c3fcbf0f96eb112c3c81a94288
Joobee <joobee97@gmail.com>
2024-02-15 16:38:48 -0800

    added test code

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import streamlit as st
 
+"hello world"
+
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
