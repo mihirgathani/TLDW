ca12a6855d800c9f255ecad481391b309e622304
Joobee <joobee97@gmail.com>
2024-02-21 16:08:23 -0800

    pulled utils modules

--- pages/0_Test.py
+++ pages/0_Test.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils import getTranscript, get_ai_extract
 
 st.title("TL;DW")
 st.caption("🚀 Get a TED Talk Recommendation based on your interest!")
@@ -17,3 +18,15 @@ txt = st.text_area(
 
 st.write(f'You wrote {len(txt)} characters.')
 
+
+def main():
+    # video_url = input("Please enter the YouTube video URL: ")
+    transcript = getTranscript(title)
+    summary, _, _ = get_ai_extract("Summarize the following transcript in 150 words: ", transcript)
+    keywords, _, _ = get_ai_extract("Generate the top 10 most important keywords: ", transcript)
+    print(summary)
+    print(keywords)
+
+
+if __name__ == '__main__':
+    main()
