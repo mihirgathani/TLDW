710b126aa1ba1468114ddef743e69b515ce67e1d
Joobee <joobee97@gmail.com>
2024-02-21 23:57:36 -0800

    implemented api backend code to streamlit

--- pages/0_Test.py
+++ pages/0_Test.py
@@ -1,32 +1,46 @@
 import streamlit as st
+import time
 from utils import getTranscript, get_ai_extract
 
-st.title("TL;DW")
+st.title("📝 TL;DW ")
 st.caption("🚀 Get a TED Talk Recommendation based on your interest!")
 
-title = st.text_input('Youtube URL', 'Input your interested YT video!')
+title = st.text_input('Youtube URL')
 
+if title:
+    with st.spinner('Getting transcript...'):
+        transcript = getTranscript(title)
+    st.success('Transcript is ready!')
 
-txt = st.text_area(
-    "Text to analyze",
-    "It was the best of times, it was the worst of times, it was the age of "
-    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
-    "was the epoch of incredulity, it was the season of Light, it was the "
-    "season of Darkness, it was the spring of hope, it was the winter of "
-    "despair, (...)",
-    )
+    if transcript:
+        st.header("Transcript")
+        st.write(transcript)
 
-st.write(f'You wrote {len(txt)} characters.')
+    st.divider()
 
+    with st.spinner('Summarizing using Gemini...'):
+        summary, _, _ = get_ai_extract("Summarize the following transcript in 150 words: ", transcript)
 
-def main():
-    # video_url = input("Please enter the YouTube video URL: ")
-    transcript = getTranscript(title)
-    summary, _, _ = get_ai_extract("Summarize the following transcript in 150 words: ", transcript)
-    keywords, _, _ = get_ai_extract("Generate the top 10 most important keywords: ", transcript)
-    print(summary)
-    print(keywords)
+    st.success('Summary is ready!')
 
+    if summary:
+        st.header("Summary")
+        st.write(summary)
+        # st.text_area(summary)
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     # video_url = input("Please enter the YouTube video URL: ")
+#     transcript = getTranscript(title)
+#     txt = st.text_area(transcript)
+#     summary, _, _ = get_ai_extract("Summarize the following transcript in 150 words: ", transcript)
+#     # keywords, _, _ = get_ai_extract("Generate the top 10 most important keywords: ", transcript)
+#     # print(summary)
+#     # print(keywords)
+#     return transcript
+
+# txt = st.text_area(transcript)
+
+# st.write(f'You wrote {len(txt)} characters.')
+
+# if __name__ == '__main__':
+#     main()
