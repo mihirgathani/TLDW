451f9d6b184644a1339529a9867396cf0169486d
mihirgathani <gmihir@uw.edu>
2024-03-04 18:01:52 -0800

    Updated code to handle Podcasts, and added associated embeddings

--- utils/Bert/bert_preprocess_TEDtalks.py
+++ utils/Bert/bert_preprocess_TEDtalks.py
@@ -1,63 +1,28 @@
 import torch
-from transformers import BertTokenizer, BertModel
+import pandas as pd
+from sentence_transformers import SentenceTransformer
 import pandas as pd
 
-from sentence_transformers import SentenceTransformer, util
-import pandas as pd
+def preprocess_bert(ted_or_podcast):
 
-def preprocess_bert():
-    # Load pre-trained SBERT model
+    if ted_or_podcast == "ted":
+        # Load TED Talks Dataset
+        df = pd.read_csv(r"C:\Users\mihir\Downloads\ted_talks_en.csv\ted_talks_en.csv") # Update file location
+        transcripts = df["transcript"].tolist()  # List of transcripts
+    else:
+        # Load Podcast Dataset
+        df = pd.read_csv(r"C:\Users\mihir\Downloads\skeptoid_transcripts.csv\skeptoid_transcripts.csv") # Update file location
+        transcripts = df["text"].tolist()  # List of transcripts
+
+    # Load pre-trained Roberta model
     model_name = 'stsb-roberta-large'
     model = SentenceTransformer(model_name)
 
-    df = pd.read_csv(r"C:\Users\mihir\Downloads\ted_talks_en.csv\ted_talks_en.csv")
-    ted_talks_descriptions = df["transcript"].tolist()  # List of transcripts of TED Talks
-
-    # Generate SBERT embeddings for TED Talks dataset
-    ted_embeddings = model.encode(ted_talks_descriptions, show_progress_bar=True)
+    # Generate Roberta embeddings for dataset
+    embeddings = model.encode(transcripts, show_progress_bar=True)
 
     # Save the embeddings to a file
-    torch.save(ted_embeddings, 'ted_sbert_embeddings.pt')
-
-# ----------------------------------------------------------------
-
-# def preprocess_bert():
-#     # Load pre-trained BERT model and tokenizer
-#     model_name = 'bert-large-uncased'
-#     tokenizer = BertTokenizer.from_pretrained(model_name)
-#     model = BertModel.from_pretrained(model_name)
-
-#     df = pd.read_csv(r"C:\Users\mihir\Downloads\Check_TED.csv")
-#     ted_talks_descriptions = df["transcript"].tolist()  # List of short descriptions of TED Talks
-
-#     # Generate BERT embeddings for TED Talks dataset
-#     ted_embeddings = []
-#     for ted_transcript in ted_talks_descriptions:
-#         # Split the transcript into chunks based on its length
-#         tokenized_chunks = tokenizer.tokenize(ted_transcript)
-#         max_chunk_len = 512  # Maximum length for BERT input
-#         chunks = [tokenized_chunks[i:i+max_chunk_len] for i in range(0, len(tokenized_chunks), max_chunk_len)]
-
-#         # Generate BERT embeddings for each chunk and average them
-#         chunk_embeddings = []
-#         for chunk in chunks:
-#             # Convert the chunk into tokens and pad if necessary
-#             encoded_chunk = tokenizer.encode(chunk, add_special_tokens=True, max_length=max_chunk_len, truncation=True)
-#             padded_chunk = encoded_chunk + [0] * (max_chunk_len - len(encoded_chunk))
-
-#             # Convert the chunk into a tensor and generate embeddings
-#             input_ids = torch.tensor(padded_chunk).unsqueeze(0)  # Add batch dimension
-#             with torch.no_grad():
-#                 outputs = model(input_ids)
-#             chunk_embedding = outputs.last_hidden_state.mean(dim=1).squeeze()
-#             chunk_embeddings.append(chunk_embedding)
-
-#         # Average the embeddings of all chunks
-#         ted_transcript_embedding = torch.stack(chunk_embeddings).mean(dim=0)
-#         ted_embeddings.append(ted_transcript_embedding)
-
-#     # Convert ted_embeddings list to a tensor
-#     ted_embeddings_tensor = torch.stack(ted_embeddings)
-
-#     # Save the embeddings tensor to a file
-#     torch.save(ted_embeddings_tensor, 'ted_embeddings.pt')
+    if ted_or_podcast == "ted":
+        torch.save(embeddings, 'ted_sbert_embeddings.pt')
+    else:
+        torch.save(embeddings, 'podcast_sbert_embeddings.pt')
--- utils/Sentence_Trans/sentTrans_get_recommedations.py
+++ utils/Sentence_Trans/sentTrans_get_recommedations.py
@@ -5,18 +5,26 @@ import numpy as np
 import pandas as pd
 
 # Function to get recommendations using precomputed embeddings
-def getSentTransRecs(input_transcript):
+def getSentTransRecs(input_transcript, ted_or_podcast):
+    
+    if ted_or_podcast == "ted":
+        # Load TED Talks Dataset
+        df = pd.read_csv(r"C:\Users\mihir\Downloads\ted_talks_en.csv\ted_talks_en.csv") # Update file location
+        with open('ted_sentTrans_embeddings.pkl', 'rb') as f:
+            embeddings = pickle.load(f)
+        transcripts = df["transcript"].tolist()
+    else:
+        # Load Podcast Dataset
+        df = pd.read_csv(r"C:\Users\mihir\Downloads\skeptoid_transcripts.csv\skeptoid_transcripts.csv") # Update file location
+        with open('podcast_sentTrans_embeddings.pkl', 'rb') as f:
+            embeddings = pickle.load(f)
+        transcripts = df["text"].tolist()
+
+    titles = df["title"].tolist()
 
     # Load Sentence Transformer model
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
     model = SentenceTransformer(model_name)
-    
-    df = pd.read_csv(r"C:\Users\mihir\Downloads\Check_TED.csv")
-    ted_titles = df["title"].tolist()
-    
-    # Load TED embeddings from file
-    with open('ted_sentTrans_embeddings.pkl', 'rb') as f:
-        ted_embeddings = pickle.load(f)
 
     # Encode user input in chunks
     chunk_size = 256
@@ -24,17 +32,17 @@ def getSentTransRecs(input_transcript):
     user_input_embedding = np.mean([model.encode(chunk) for chunk in chunks], axis=0)
 
     # Compute cosine similarity between user input and TED transcripts
-    similarity_scores = cosine_similarity([user_input_embedding], ted_embeddings)[0]
+    similarity_scores = cosine_similarity([user_input_embedding], embeddings)[0]
 
     # Rank transcripts based on similarity scores
-    ranked_transcripts = sorted(zip(ted_titles, similarity_scores), key=lambda x: x[1], reverse=True)
+    ranked_transcripts = sorted(zip(titles, similarity_scores), key=lambda x: x[1], reverse=True)
 
     # Get top 3 recommendations
     top_recommendations = ranked_transcripts[:3]
 
     # Print top 3 recommendations
     print("-------------------------------------------------------------")
-    print("Top 3 Recommendations - Sentence Transformer -> all-MiniLM-L6-v2:")
+    print(f"Top 3 Recommendations for {ted_or_podcast} - Model all-MiniLM-L6-v2:")
     for i, (recommendation, similarity_score) in enumerate(top_recommendations, 1):
         print(f"Recommendation {i}")
         print(f"Title: {recommendation}")
