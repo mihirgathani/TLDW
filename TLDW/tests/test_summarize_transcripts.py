12c86528bf18051da9d3478fff3749ee21c6221e
anuragagarwal1910 <anurag96@uw.edu>
2024-03-09 14:02:46 -0800

    Added mock test for API

--- TLDW/tests/test_summarize_transcripts.py
+++ TLDW/tests/test_summarize_transcripts.py
@@ -4,6 +4,8 @@ Test suite for the summarize_transcripts module.
 
 import unittest
 from utils.summarize_transcripts import get_ai_extract
+from unittest.mock import patch
+from unittest import mock
 
 
 class TestGetAIExtract(unittest.TestCase):
@@ -24,3 +26,23 @@ class TestGetAIExtract(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             get_ai_extract('hello','')
+
+    @patch('utils.summarize_transcripts.genai_model.generate_content')
+    def test_api_call(self, mock_generate_content):
+        """
+        Test API call behavior.
+        """
+
+        mock_response = mock.Mock(return_value=None)
+        mock_response.candidates = [
+            mock.Mock(content=mock.Mock(parts=[mock.Mock(text='rocks')])),
+            # You can add more mock candidates if needed
+        ]
+
+        mock_generate_content.return_value = mock_response
+
+        result = get_ai_extract('What is the last word of the sentence', 'tldw rocks')
+        self.assertEqual(result, 'rocks')
+
+if __name__ == '__main__':
+    unittest.main()
