f53fe23293ae86216c9a325d4713a372ff35a0a5
mihirgathani <gmihir@uw.edu>
2024-03-09 14:47:24 -0800

    Added updated Anurag files since that branch had issues

--- TLDW/tests/test_bert_get_recommendations.py
+++ TLDW/tests/test_bert_get_recommendations.py
@@ -1,116 +1,116 @@
-"""
-This fule contains unit tests for the functionality provided
-by the bert_get_recommendations module. It includes tests to ensure
-the correctness and robustness of the get_bert_recs function.
-"""
+# """
+# This fule contains unit tests for the functionality provided
+# by the bert_get_recommendations module. It includes tests to ensure
+# the correctness and robustness of the get_bert_recs function.
+# """
 
-import unittest
-import os
-from utils.bert_get_recommendations import get_bert_recs
+# import unittest
+# import os
+# from utils.bert_get_recommendations import get_bert_recs
 
-class TestBertGetRecommendations(unittest.TestCase):
-    """
-    TestBertGetRecommendations class contains unit tests for the
-    bert_get_recommendations module.
-    """
+# class TestBertGetRecommendations(unittest.TestCase):
+#     """
+#     TestBertGetRecommendations class contains unit tests for the
+#     bert_get_recommendations module.
+#     """
 
-    user_transcript = "This is a test transcript."
+#     user_transcript = "This is a test transcript."
 
-    with open('tests/user_transcript1.txt', 'r') as file:
-        user_transcript1 = file.read()
+#     with open('tests/user_transcript1.txt', 'r') as file:
+#         user_transcript1 = file.read()
 
-    def test_smoke_file_exists_ted(self):
-        """
-        Test if the required embedding file for TED Talks exists.
-        """
-        file_path = './ted_sbert_embeddings.pt'
-        self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
+#     def test_smoke_file_exists_ted(self):
+#         """
+#         Test if the required embedding file for TED Talks exists.
+#         """
+#         file_path = './ted_sbert_embeddings.pt'
+#         self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
 
-    def test_smoke_data_exists_ted(self):
-        """
-        Test if the required data file for TED Talks exists.
-        """
-        file_path = './ted_talks_en.csv'
-        self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
+#     def test_smoke_data_exists_ted(self):
+#         """
+#         Test if the required data file for TED Talks exists.
+#         """
+#         file_path = './ted_talks_en.csv'
+#         self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
 
-    def test_smoke_file_exists_podcast(self):
-        """
-        Test if the required embedding file for podcast exists.
-        """
-        file_path = './podcast_sbert_embeddings.pt'
-        self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
+#     def test_smoke_file_exists_podcast(self):
+#         """
+#         Test if the required embedding file for podcast exists.
+#         """
+#         file_path = './podcast_sbert_embeddings.pt'
+#         self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
 
-    def test_smoke_data_exists_podcast(self):
-        """
-        Test if the required data file for podcast exists.
-        """
-        file_path = './skeptoid_transcripts.csv'
-        self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
+#     def test_smoke_data_exists_podcast(self):
+#         """
+#         Test if the required data file for podcast exists.
+#         """
+#         file_path = './skeptoid_transcripts.csv'
+#         self.assertTrue(os.path.exists(file_path), f"The file {file_path} does not exist.")
 
-    def test_smoke_ted(self):
-        """
-        Test the functionality of get_bert_recs for TED Talks.
-        """
-        ted_or_podcast = "ted"
-        recommendations = get_bert_recs(self.user_transcript, ted_or_podcast)
-        print("Smoke test - ted: passed.")
-        self.assertFalse(recommendations.empty)
+#     def test_smoke_ted(self):
+#         """
+#         Test the functionality of get_bert_recs for TED Talks.
+#         """
+#         ted_or_podcast = "ted"
+#         recommendations = get_bert_recs(self.user_transcript, ted_or_podcast)
+#         print("Smoke test - ted: passed.")
+#         self.assertFalse(recommendations.empty)
 
-    def test_smoke_podcast(self):
-        """
-        Test the functionality of get_bert_recs for Podcasts.
-        """
-        ted_or_podcast = "podcast"
-        recommendations = get_bert_recs(self.user_transcript, ted_or_podcast)
-        print("Smoke test - podcast: passed.")
-        self.assertFalse(recommendations.empty)
+#     def test_smoke_podcast(self):
+#         """
+#         Test the functionality of get_bert_recs for Podcasts.
+#         """
+#         ted_or_podcast = "podcast"
+#         recommendations = get_bert_recs(self.user_transcript, ted_or_podcast)
+#         print("Smoke test - podcast: passed.")
+#         self.assertFalse(recommendations.empty)
 
-    def test_one_shot_ted(self):
-        """
-        Test the functionality of get_bert_recs for ted talks, given a specific transcript.
-        """
-        ted_or_podcast = "ted"
-        recommendations = get_bert_recs(self.user_transcript1, ted_or_podcast)
-        print("One-shot test - ted: passed.")
-        self.assertTrue(recommendations.iloc[0]['title'] == "Averting the climate crisis")
+#     def test_one_shot_ted(self):
+#         """
+#         Test the functionality of get_bert_recs for ted talks, given a specific transcript.
+#         """
+#         ted_or_podcast = "ted"
+#         recommendations = get_bert_recs(self.user_transcript1, ted_or_podcast)
+#         print("One-shot test - ted: passed.")
+#         self.assertTrue(recommendations.iloc[0]['title'] == "Averting the climate crisis")
 
-    def test_one_shot_podcast(self):
-        """
-        Test the functionality of get_bert_recs for podcasts, given a specific transcript.
-        """
-        ted_or_podcast = "podcast"
-        recommendations = get_bert_recs(self.user_transcript1, ted_or_podcast)
-        print("One-shot test - podcast: passed.")
-        self.assertTrue(recommendations.iloc[0]['title'] == "Email Myths")
+#     def test_one_shot_podcast(self):
+#         """
+#         Test the functionality of get_bert_recs for podcasts, given a specific transcript.
+#         """
+#         ted_or_podcast = "podcast"
+#         recommendations = get_bert_recs(self.user_transcript1, ted_or_podcast)
+#         print("One-shot test - podcast: passed.")
+#         self.assertTrue(recommendations.iloc[0]['title'] == "Email Myths")
 
-    # Edge Tests
-    def test_edge_value_error_arg1(self):
-        """
-        Test handling of ValueError when user_transcript is empty.
-        """
-        with self.assertRaises(ValueError):
-            get_bert_recs("", "ted")
+#     # Edge Tests
+#     def test_edge_value_error_arg1(self):
+#         """
+#         Test handling of ValueError when user_transcript is empty.
+#         """
+#         with self.assertRaises(ValueError):
+#             get_bert_recs("", "ted")
 
-    def test_edge_type_error_arg1(self):
-        """
-        Test handling of TypeError when user_transcript is not a string.
-        """
-        with self.assertRaises(TypeError):
-            get_bert_recs(1, "ted")
+#     def test_edge_type_error_arg1(self):
+#         """
+#         Test handling of TypeError when user_transcript is not a string.
+#         """
+#         with self.assertRaises(TypeError):
+#             get_bert_recs(1, "ted")
 
-    def test_edge_value_error_arg2(self):
-        """
-        Test handling of ValueError when ted_or_podcast is invalid.
-        """
-        with self.assertRaises(ValueError):
-            get_bert_recs("Correct", "empty")
+#     def test_edge_value_error_arg2(self):
+#         """
+#         Test handling of ValueError when ted_or_podcast is invalid.
+#         """
+#         with self.assertRaises(ValueError):
+#             get_bert_recs("Correct", "empty")
 
-    def test_edge_type_error_arg2(self):
-        """
-        Test handling of TypeError when ted_or_podcast is not a string.
-        """
-        with self.assertRaises(TypeError):
-            get_bert_recs("Correct", 8)
+#     def test_edge_type_error_arg2(self):
+#         """
+#         Test handling of TypeError when ted_or_podcast is not a string.
+#         """
+#         with self.assertRaises(TypeError):
+#             get_bert_recs("Correct", 8)
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
--- TLDW/utils/process_transcripts.py
+++ TLDW/utils/process_transcripts.py
@@ -1,38 +1,156 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+# from youtube_transcript_api import YouTubeTranscriptApi
+
+# DEBUG_MODE = False
+
+# def get_video_id(youtube_url):
+#     # Split the URL by "=" sign
+#     parts = youtube_url.split("v=")
+#     # Take the last part of the resulting list
+#     video_id = parts[-1]
+#     if DEBUG_MODE:
+#         print("Video ID: " + video_id)
+#     return video_id
+
+# def get_video_transcript(video_id):
+#     try:
+#         transcriptDict = YouTubeTranscriptApi.get_transcript(video_id)
+#         formattedTranscript = formatTranscript(transcriptDict)
+#         if DEBUG_MODE:
+#             print("Processed transcript: " + formattedTranscript)
+#         return formattedTranscript
+#     except Exception as e:
+#         print("Failed to get transcript")
+#         if DEBUG_MODE:
+#             print(f"Error: {e}")
+#         return None
+
+# def formatTranscript(transcriptDict):
+#     transcript_string = ""
+#     for item in transcriptDict:
+#         transcript_string += item['text'] + " "
+#     # Replace multiple spaces with a single space
+#     transcript_string = ' '.join(transcript_string.split())
+#     return transcript_string.strip()
+
+# def get_transcript(youtube_url):
+#     video_id = get_video_id(youtube_url)
+#     transcript = get_video_transcript(video_id)
+#     return transcript
+
+# ----------------------------------------------------------------
+
+"""
+process_transcripts.py
+
+This script provides functions to process transcripts from YouTube videos.
+"""
+
+from youtube_transcript_api import YouTubeTranscriptApi,  TranscriptsDisabled, NoTranscriptAvailable
 
 DEBUG_MODE = False
 
 def get_video_id(youtube_url):
+    """
+    Extracts the video ID from a YouTube URL.
+
+    Args:
+        youtube_url (str): The URL of the YouTube video.
+
+    Returns:
+        str: The extracted video ID.
+        
+    Raises:
+        ValueError: If the URL is not in the correct format.
+    """
+    if 'v=' not in youtube_url:
+        raise ValueError("URL is not correct")
     # Split the URL by "=" sign
     parts = youtube_url.split("v=")
     # Take the last part of the resulting list
     video_id = parts[-1]
+    # Edge conditions for unit testing:
+    if len(video_id) == 0:
+        raise ValueError("Video id for input video cannot be blank")
+    #if len(video_id) != 11:
+     #   raise ValueError("Video ID needs to have 11 characters")
+    if not video_id[:11].isalnum():
+        raise ValueError("Video ID is not correct")
     if DEBUG_MODE:
         print("Video ID: " + video_id)
     return video_id
 
 def get_video_transcript(video_id):
-    try:
-        transcriptDict = YouTubeTranscriptApi.get_transcript(video_id)
-        formattedTranscript = formatTranscript(transcriptDict)
-        if DEBUG_MODE:
-            print("Processed transcript: " + formattedTranscript)
-        return formattedTranscript
-    except Exception as e:
-        print("Failed to get transcript")
-        if DEBUG_MODE:
-            print(f"Error: {e}")
-        return None
+    """
+    Retrieves and formats the transcript for a given video ID.
 
-def formatTranscript(transcriptDict):
+    Args:
+        video_id (str): The ID of the YouTube video.
+
+    Returns:
+        str: The formatted transcript.
+        
+    Raises:
+        ValueError: If the transcript is not available in English.
+    """
+    try:
+        transcript_dict = YouTubeTranscriptApi.get_transcript(video_id, languages=['en-US', 'en'])
+        # desired_languages = {'en-US', 'en'}  # Desired languages
+        # available_languages = {segment.get('language') for segment in transcript_dict}
+        # if not desired_languages.intersection(available_languages):
+        #     raise ValueError("Transcript does not contain any of the desired languages.")
+        formatted_transcript = format_transcript(transcript_dict)
+        if DEBUG_MODE:
+            print("Processed transcript: " + formatted_transcript)
+        return formatted_transcript
+    except TranscriptsDisabled:
+        raise ValueError("Transcripts are disabled for this video.")
+    except NoTranscriptAvailable:
+        raise ValueError("No transcript available for this video.")
+    except Exception as e:
+        raise e
+   # except Exception as error:
+   #     print("Failed to get transcript")
+   #     if DEBUG_MODE:
+   #         print(f"Error: {error}")
+   #     return None
+
+def format_transcript(transcript_dict):
+    """
+    Formats the transcript dictionary into a single string.
+
+    Args:
+        transcript_dict (list): List of dictionary items representing transcript segments.
+
+    Returns:
+        str: The formatted transcript string.
+        
+    Raises:
+        ValueError: If there is no transcript associated with the video.
+    """
     transcript_string = ""
-    for item in transcriptDict:
+    for item in transcript_dict:
         transcript_string += item['text'] + " "
     # Replace multiple spaces with a single space
     transcript_string = ' '.join(transcript_string.split())
+    if len(transcript_string) == 0:
+        raise ValueError('There is no transcript associated with the video')
     return transcript_string.strip()
 
 def get_transcript(youtube_url):
+    """
+    Retrieves the transcript for a YouTube video given its URL.
+
+    Args:
+        youtube_url (str): The URL of the YouTube video.
+
+    Returns:
+        str: The formatted transcript.
+        
+    Raises:
+        ValueError: If the URL is not in the correct format or transcript retrieval fails.
+    """
+    if not youtube_url.startswith("https://www.youtube.com"):
+        raise ValueError("Video URL should start with 'https://www.youtube.com'")
     video_id = get_video_id(youtube_url)
     transcript = get_video_transcript(video_id)
     return transcript
